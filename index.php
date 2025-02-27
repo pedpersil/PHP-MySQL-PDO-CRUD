ba6027c10bc3b08808a2c9c37dacf2b40df022df
Pedro Silva <pedpersil@gmail.com>
2025-02-27 17:03:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,8 @@ if (!$auth->checkLogin()) {
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- SweetAlert2 CDN -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <style>
       .img-container {
@@ -54,6 +56,7 @@ if (!$auth->checkLogin()) {
             showCancelButton: true,
             confirmButtonText: 'Sim, excluir',
             cancelButtonText: 'Cancelar',
+            confirmButtonColor: '#FF000045'
         }).then((result) => {
             if (result.isConfirmed) {
                 // Se o usuário confirmar, redireciona para o link de exclusão
