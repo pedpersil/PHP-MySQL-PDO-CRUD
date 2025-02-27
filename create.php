07ea5e5e92b207ade4838ea21468f4c933e5b643
Pedro Silva <pedpersil@gmail.com>
2025-02-27 17:02:15 -0300

    Update create.php

--- create.php
+++ create.php
@@ -12,18 +12,6 @@ if (!$auth->checkLogin()) {
     exit();
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-
-    $user = new User();
-    if ($user->create($name, $email)) {
-        header('Location: index.php');
-    } else {
-        echo "Erro ao criar usuário.";
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +19,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- SweetAlert2 CDN -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 <style>
       .img-container {
         text-align: center;
@@ -38,6 +29,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </style>
 </head>
 <body>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $user = new User();
+
+    /*
+    if ($user->create($name, $email)) {
+        header('Location: index.php');
+    } else {
+        echo "Erro ao criar usuário.";
+    }
+    */
+
+    if ($user->create($name, $email)) {
+        // Exclusão bem-sucedida, redireciona com uma mensagem de sucesso via SweetAlert2
+        echo "<script>
+                Swal.fire({
+                    title: 'Sucesso!',
+                    text: 'Usuário criado com sucesso!',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'index.php';  // Redireciona para a página de listagem
+                    }
+                });
+              </script>";
+    } else {
+        // Caso ocorra algum erro na exclusão, redireciona com uma mensagem de erro
+        echo "<script>
+                Swal.fire({
+                    title: 'Erro!',
+                    text: 'Não foi possível criar o usuário. Tente novamente.',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'index.php';  // Redireciona para a página de listagem
+                    }
+                });
+              </script>";
+    }
+    
+}
+?>
+
 <div class="img-container">
 <img src="crud.jpg" alt="CRUD">
 </div>
