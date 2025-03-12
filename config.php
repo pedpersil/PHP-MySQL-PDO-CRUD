612f82720f6d1179a8ed888b89e660c57227ccef
Pedro Silva <pedpersil@gmail.com>
2025-03-12 14:52:46 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,12 +5,3 @@ define('DB_NAME', 'crud_pdo');
 define('DB_USER', 'root'); // Substitua pelo seu usuário
 define('DB_PASS', 'rootpassword'); // Substitua pela sua senha
 define('SESSION_NAME', 'user_session'); // Nome da sessão
-
-
-/*
-define('DB_HOST', 'sql112.ezyro.com');
-define('DB_NAME', 'ezyro_38404991_crud_pdo');
-define('DB_USER', 'ezyro_38404991'); // Substitua pelo seu usuário
-define('DB_PASS', 'P0w3rb4nk'); // Substitua pela sua senha
-define('SESSION_NAME', 'user_session'); // Nome da sessão
-*/
